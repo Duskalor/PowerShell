7389156ae17fa6d9a90b55630fa8efefbc6af5a5
duskalor <duskalor1@gmail.com>
2023-09-02 22:11:37 -0500

    tw

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,8 +7,14 @@ function w {
 function pr {
     Set-Location -Path "C:\Users\$user\Documents\Proyects\React"
 }
+function fr {
+    Set-Location -Path "C:\Users\$user\Documents\Proyects\React\frontEndMentor"
+}
 function n {
     Set-Location -Path "C:\Users\$user\Documents\Proyects\Node"
+}
+function as {
+    Set-Location -Path "C:\Users\$user\Documents\Proyects\astro"
 }
 function l {
     Set-Location -Path "C:\Users\$user\Documents\Proyects\Laravel"
@@ -28,10 +34,30 @@ function telegram {
     Set-Location -Path "F:\Juegos\Suzumiya Haruhi\asd\Telegram"
 }
 
+
 function vite {
    pnpm create vite@latest $args[0];
    Set-Location $args[0];
-   pnpm i;
+   pnpm i;   
+   code .;
+   pnpm run dev;
+}
+function vitet {
+   pnpm create vite@latest $args[0];
+   Set-Location $args[0];
+   pnpm install -D tailwindcss postcss autoprefixer;
+   npx tailwindcss init -p;
+   pnpm i;   
+   code .;
+   pnpm run dev;
+}
+
+
+
+
+function astro {
+   pnpm create astro@latest $args[0];
+   Set-Location $args[0];
    code .;
    pnpm run dev;
 }
